600666c3b741ef86f25717052a5938c15772f9bd
Beingsankey03 <72875340+Beingsankey03@users.noreply.github.com>
2021-06-25 14:44:56 +0530

    Add files via upload

---
+++ sketch_jun25a1.ino
@@ -0,0 +1,41 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2,3,4,5,6,7);
+const int trigPin = 12;
+const int echoPin = 11;
+long duration;
+long distanceCm;
+float liquid;
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16,2);
+  pinMode(trigPin ,OUTPUT);
+  pinMode(echoPin ,INPUT);
+  lcd.setCursor(0,0);
+  lcd.print("    Distance   ");
+  lcd.setCursor(0,1);
+  lcd.print("   Measurement   ");
+  delay(2000);
+  lcd.clear();
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin , LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distanceCm = duration*0.034/2;
+  lcd.setCursor(0,0);
+  lcd.print("Distance Measur.");
+  delay(10);
+  lcd.setCursor(0,1);
+  lcd.print("Distance:");
+  lcd.print(distanceCm);
+  lcd.print(" On ");
+  delay(10);
+}
